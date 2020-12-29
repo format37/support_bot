f19aade2caa4f7906c44aa06f549bfd0321d576d
Alex <format37@gmail.com>
2020-12-29 14:37:54 +0300

    modified:   relay.py

--- relay.py
+++ relay.py
@@ -1,6 +1,9 @@
 import asyncio
 from aiohttp import web
 import urllib
+from requests.auth import HTTPBasicAuth
+import requests
+import json
 
 WEBHOOK_PORT = 8084
 WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
@@ -17,6 +20,21 @@ async def call_relay(request):
         user = urllib.parse.quote_plus(request.rel_url.query['user'])
         text = urllib.parse.quote_plus(request.rel_url.query['text'])
         print(user, text)
+
+        with open('auth.login', 'r') as file:
+            login = file.read().replace('\n', '')
+            file.close()
+        with open('auth.pass', 'r') as file:
+            password = file.read().replace('\n', '')
+            file.close()
+
+        auth = HTTPBasicAuth(login, password)
+        url = "http://10.2.4.123/productionCRM/hs/telegram/api/v1/post/request"
+
+        params = str(user) + str(text)
+        r = requests.post(url, json=json.dumps(params), auth=auth)
+        print(r.text)
+
 
     except Exception as e:
         print('Error: ' + str(e) )
