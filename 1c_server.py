eb9b3353f371c296245d30f9a1e4aac97488376c
Alex <format37@gmail.com>
2020-12-29 10:37:33 +0300

    modified:   1c_server.py

--- 1c_server.py
+++ 1c_server.py
@@ -21,7 +21,8 @@ SSL_PATH = '/etc/letsencrypt/live/service.icecorp.ru/' # execute as root
 
 WEBHOOK_HOST = 'service.icecorp.ru'
 WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
-WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
+#WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
+WEBHOOK_LISTEN = '95.165.209.182'
 
 WEBHOOK_SSL_CERT = SSL_PATH+'fullchain.pem'  # Path to the ssl certificate
 WEBHOOK_SSL_PRIV = SSL_PATH+'privkey.pem'  # Path to the ssl private key
