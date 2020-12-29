8c321e656e3e02db8f27908359a422a8a2f66300
Alex <format37@gmail.com>
2020-12-29 10:43:30 +0300

    modified:   1c_server.py

--- 1c_server.py
+++ 1c_server.py
@@ -20,7 +20,8 @@ SCRIPT_PATH	= '/home/dvasilev/projects/support_bot/'
 SSL_PATH = '/etc/letsencrypt/live/service.icecorp.ru/' # execute as root
 
 WEBHOOK_HOST = 'service.icecorp.ru'
-WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
+#WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
+WEBHOOK_PORT = 88  # 443, 80, 88 or 8443 (port need to be 'open')
 WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
 #WEBHOOK_LISTEN = '95.165.209.182'
 
